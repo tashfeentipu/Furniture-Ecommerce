06d172c3d287e320b1074fc9f6523e4180ab59be
tashfeentipu <tashfeentipu@hotmail.com>
2023-04-28 10:48:47 +0500

    --commit: Container Ready--

--- container/webpack/webpack.dev.js
+++ container/webpack/webpack.dev.js
@@ -1,9 +1,11 @@
 const webpack = require('webpack')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
 module.exports = {
   mode: 'development',
   devtool: 'cheap-module-source-map',
   plugins: [
+    new ReactRefreshWebpackPlugin(),
     new webpack.DefinePlugin({
       'process.env.name': JSON.stringify('Vishwas'),
     }),
